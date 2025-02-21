543b5e2c6e77b408983a0e6f2e1880e180cc59b4
daniel <da@dataki.co>
2025-02-21 09:56:54 -0300

    commit home 21/02/25

--- src/components/image-grid/ImageGrid.tsx
+++ src/components/image-grid/ImageGrid.tsx
@@ -4,7 +4,7 @@ import './ImageGrid.css';
 export default function ImageGrid() {
   return (
     <div className="mx-auto w-full sm:mb-16 min-h-screen overflow-hidden max-sm:pt-10">
-      <div className="max-sm:hidden max-sm:mt-12 sm:mt-14 ">
+      <div className="max-sm:hidden max-sm:mt-12 sm:mt-14 our-cases-major ">
         <h2 className="text-center text-cases font-Poppins text-black font-light sm:pb-32 lg:pb-44 uppercase">
           our CASES
         </h2>
@@ -24,7 +24,7 @@ export default function ImageGrid() {
           <Image
             src="/images-grid/busy-1.png"
             alt="busy-1.png"
-            className="sm:w-[212px] lg:w-[285px] lg:h-[198px] xl:w-[400px] sm:h-[150px] xl:h-[278px]"
+            className="sm:w-[212px] lg:w-[285px] lg:h-[198px] xl:w-[400px] sm:h-[150px] xl:h-[278px] width-major"
             width={400}
             height={278}
           />
@@ -34,7 +34,7 @@ export default function ImageGrid() {
           <Image
             src="/images-grid/Maskgroup2.png"
             alt="Mask group 2"
-            className="sm:w-[212px] lg:w-[285px] lg:h-[198px] xl:w-[400px] sm:h-[150px] xl:h-[278px] sm:-translate-y-1/2"
+            className="sm:w-[212px] lg:w-[285px] lg:h-[198px] xl:w-[400px] sm:h-[150px] xl:h-[278px] width-major sm:-translate-y-1/2"
             width={400}
             height={278}
           />
@@ -44,7 +44,7 @@ export default function ImageGrid() {
           <Image
             src="/images-grid/Mask-group33.png"
             alt="Mask group 3"
-            className="max-sm:h-[173px] sm:w-[212px] lg:w-[285px] lg:h-[198px] xl:w-[400px] sm:h-[150px] xl:h-[278px] sm:-translate-y-1/3"
+            className="max-sm:h-[173px] sm:w-[212px] lg:w-[285px] lg:h-[198px] xl:w-[400px] sm:h-[150px] xl:h-[278px] width-major sm:-translate-y-1/3"
             width={400}
             height={278}
           />
@@ -53,8 +53,8 @@ export default function ImageGrid() {
 
       {/* Sección 'Work' */}
       <div className="max-sm:hidden absolute inset-0 flex items-center justify-center">
-        <h2 className="absolute text-work font-bold font-antonio uppercase top-[37%] xl:top-[34%] left-1/2 -translate-x-1/2 -translate-y-1/2">work</h2>
-        <h3 className="absolute z-10  text-Showcase font-light font-signature text-easternBlue top-[44%] xl:top-[40%] left-[57%] -translate-x-1/2 -translate-y-1/2">
+        <h2 className="absolute text-work font-bold font-antonio uppercase top-[22%] lg:top-[28%] xl:top-[32%] 2xl:top-[34%] left-1/2 -translate-x-1/2 -translate-y-1/2 work-major">work</h2>
+        <h3 className="absolute z-10  text-Showcase font-light font-signature text-easternBlue top-[27%] lg:top-[33%] xl:top-[38%] 2xl:top-[40%] left-[57%] -translate-x-1/2 -translate-y-1/2 work-showcase">
           Showcase
         </h3>
       </div>
@@ -65,7 +65,7 @@ export default function ImageGrid() {
         <Image
           src="/images-grid/Mask-group4.png"
           alt="Mask group 4"
-          className="sm:w-[212px] lg:w-[285px] lg:h-[198px] xl:w-[400px] sm:h-[150px] xl:h-[278px]"
+          className="sm:w-[212px] lg:w-[285px] lg:h-[198px] xl:w-[400px] sm:h-[150px] xl:h-[278px] width-major"
           width={400}
           height={278}
         />
@@ -73,7 +73,7 @@ export default function ImageGrid() {
           <Image
             src="/images-grid/beacon-beach5.png"
             alt="Mask group 2"
-            className="sm:w-[212px] lg:w-[285px] lg:h-[198px] xl:w-[400px]  sm:h-[150px] xl:h-[278px] sm:-translate-y-1/2"
+            className="sm:w-[212px] lg:w-[285px] lg:h-[198px] xl:w-[400px]  sm:h-[150px] xl:h-[278px] width-major sm:-translate-y-1/2"
             width={400}
             height={278}
           />
@@ -82,7 +82,7 @@ export default function ImageGrid() {
           <Image
             src="/images-grid/miroshnichenko66.png"
             alt="Mask group 3"
-            className="max-sm:h-[173px] sm:w-[212px] lg:w-[285px] lg:h-[198px] xl:w-[400px] sm:h-[150px] xl:h-[278px] sm:-translate-y-[-20%]"
+            className="max-sm:h-[173px] sm:w-[212px] lg:w-[285px] lg:h-[198px] xl:w-[400px] sm:h-[150px] xl:h-[278px] width-major sm:-translate-y-[-20%]"
             width={400}
             height={278}
           />
@@ -90,11 +90,11 @@ export default function ImageGrid() {
       </div>
 
       {/* Tercer grupo de imágenes */}
-      <div className="flex max-sm:flex-col justify-center items-center sm:justify-between sm:items-end max-sm:gap-8 max-sm:mb-8">
+      <div className="flex max-sm:flex-col justify-center items-center sm:justify-between sm:items-end max-sm:gap-8 ">
         <Image
           src="/images-grid/Macbook-Claro7.png"
           alt="Macbook image"
-          className="sm:w-[212px] lg:w-[285px] lg:h-[198px] xl:w-[400px] sm:h-[150px] xl:h-[278px]"
+          className="sm:w-[212px] lg:w-[285px] lg:h-[198px] xl:w-[400px] sm:h-[150px] xl:h-[278px] width-major"
           width={400}
             height={278}
         />
@@ -102,7 +102,7 @@ export default function ImageGrid() {
           <Image
             src="/images-grid/TC2K_ENTRADA8.png"
             alt="TC2K image"
-            className="sm:w-[212px] lg:w-[285px] lg:h-[198px] xl:w-[400px] sm:h-[150px] xl:h-[278px] sm:-translate-y-1/2"
+            className="sm:w-[212px] lg:w-[285px] lg:h-[198px] xl:w-[400px] sm:h-[150px] xl:h-[278px] width-major sm:-translate-y-1/2"
             width={400}
             height={278}
           />
@@ -111,7 +111,7 @@ export default function ImageGrid() {
           <Image
             src="/images-grid/Mask-group-99.png"
             alt="Mask group 3"
-            className="max-sm:h-[173px] sm:w-[212px] lg:w-[285px] lg:h-[198px] xl:w-[400px] sm:h-[150px] xl:h-[278px]"
+            className="max-sm:h-[173px] sm:w-[212px] lg:w-[285px] lg:h-[198px] xl:w-[400px] sm:h-[150px] xl:h-[278px] width-major"
             width={400}
             height={278}
           />
