7873a596df5660778da818afe86bdf119caf3475
daDataki <da@dataki.co>
2025-03-13 14:14:32 -0300

    correcciones 13/03/251

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,20 +1,13 @@
 
-import Image from "next/image";
 import "./about.css";
-import Header from "../../components/header1/Header1";
+
 import Footer from "../../components/footer/Footer"
 
 
 export default function About() {
   return (
     <div>
-      <Header
-        className="text-white"
-        logoSrc="/images-proyecto/logo.png"
-        menuIconSrc="/images-proyecto/menuWhite.png"
-        logoSrcOpen="/images-proyecto/logo.png"
-        menuCloseIcon="/images-proyecto/closeMenu.svg"
-      />
+     
       
       <div className="bg-black pt-12   sm:pt-20">
         <div className="pl-12 sm:pl-24">
