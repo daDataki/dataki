064aeda549e2c1b6105db2daf7741aa4433ffb69
daDataki <da@dataki.co>
2025-05-13 17:28:06 -0300

    commit 13/5/20253

--- src/app/vida-estilo/page.tsx
+++ src/app/vida-estilo/page.tsx
@@ -51,6 +51,12 @@ export default function VidaEstilo() {
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
+  useEffect(() => {
+    const img = document.querySelector('.scrollable-image') as HTMLImageElement;
+    if (img && img.complete) {
+      img.style.animationPlayState = 'running';
+    }
+  }, []);
 
   return (
     <>
@@ -132,28 +138,35 @@ export default function VidaEstilo() {
               <h2 className="uppercase text-bordered-branding sm:left-3">graphic</h2>
             </div>
             <div className=' w-full flex justify-center'>
-              <div className="relative w-[70%] z-20">
-                {/* Contenedor con overflow-hidden para simular el marco de la pantalla */}
-                <div className="absolute bg-blue w-[82%] h-[53%] top-[8%] left-[9%] overflow-hidden ">
-                  <div className="relative w-full h-full">
-                    {/* Imagen con animación de desplazamiento */}
-                    <img
-                      className="scrollable-image"
-                      src="/images-proyecto/vida-est-image-1-2.png"
-                      alt="vida-estilo"
-                    />
-                  </div>
-                </div>
+            <div className="relative w-[70%] z-20 mx-auto top-[-10vw] ">
+                        {/* Contenedor con overflow-hidden para simular el marco de la pantalla */}
+                        <div className="absolute bg-blue w-[73.2%] h-[75%] top-[6%] left-[13.8%] overflow-hidden">
+                            <div className="relative w-full h-full">
+                                {/* Imagen con animación de desplazamiento */}
+                                <Image
+                                    className="absolute w-full scrollable-image"
+                                    src="/images-proyecto/vida-est-image-1.png"
+                                    alt="vida-estilo"
+                                    width={800}
+                                    height={1111}
+                                    layout="intrinsic"
+                                    priority={true}
+                                />
+                            </div>
+                        </div>
 
-                {/* Imagen estática de la pantalla */}
-                <Image
-                  src="/images-proyecto/imac.png"
-                  alt="iMac"
-                  width={800}
-                  height={1111}
-                  priority
-                />
-              </div>
+                        {/* Imagen estática de la pantalla */}
+                        <Image
+                            className="z-10 w-full"
+                            src="/images-proyecto/Claro-Mockup.png"
+                            alt="vida-estilo"
+                            width={800}
+                            height={1111}
+                            layout="intrinsic"
+                            priority={true}
+                        />
+                    </div>
+
               <div id="design" className="absolute bottom-24 w-fit text-start left-12">
                 <h2 className="uppercase text-bordered-branding sm:left-3">design</h2>
               </div>
